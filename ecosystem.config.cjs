fe21d46df5e0bf7eea0baab3c02a56b2ed30ff0b
Miftahu Arif <arieftheluffy@gmail.com>
2026-05-12 09:26:32 +0800

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,19 +5,7 @@ module.exports = {
 			script: 'build/index.js',
 			instances: 'max', // Gunakan semua CPU cores yang tersedia
 			exec_mode: 'cluster',
-			env: {
-				NODE_ENV: 'production',
-				PORT: 5000,
-				// Database configuration
-				DB_HOST: 'localhost',
-				DB_USER: 'your_db_user',
-				DB_PASSWORD: 'your_db_password',
-				DB_NAME: 'glx_link',
-				DB_PORT: 3306,
-				// Application configuration
-				ORIGIN: 'https://yourdomain.com'
-				// Add other environment variables here
-			},
+			env: '.env',
 			// Logging
 			error_file: './logs/pm2-error.log',
 			out_file: './logs/pm2-out.log',
